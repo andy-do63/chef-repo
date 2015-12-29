a4df9a3af54f24a63ae85afb1f08d8177aa36d6d
Andy Do <ndo36@hotmail.com>
2015-12-29 15:41:28 -0500

    Comment out the KERN_DIR as to don't know what's the correct kernel version

--- yum-repo/virtualbox-install.sh
+++ yum-repo/virtualbox-install.sh
@@ -14,7 +14,7 @@
     yum install kernel-headers kernel-devel fontforge binutils glibc-headers glibc-devel
     
 # Setup Environment Variables
-    export KERN_DIR=/usr/src/kernels/2.6.32-504.3.3.el6.x86_64
+    # export KERN_DIR=/usr/src/kernels/2.6.32-504.3.3.el6.x86_64
     
 # Install Virtualbox & Setup
     yum install VirtualBox-5.0
