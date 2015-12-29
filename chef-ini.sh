46e7aafde30ea91ee05f6d1c3045f9b06f712f77
Andy Do <ndo36@hotmail.com>
2015-12-29 14:08:45 -0500

    Add nginx cookbook download

--- chef-ini.sh
+++ chef-ini.sh
@@ -21,3 +21,17 @@
 # Config knife
     cd ~/chef-repo/.chef
     echo "cookbook_path '~/chef-repo/cookbooks'" >> knife.rb
+    
+# Download nginx & dependency cookbooks
+    cd ~/chef-repo/cookbooks
+    knife cookbook site download nginx
+    knife cookbook site download apt
+    knife cookbook site download bluepill
+    knife cookbook site download build-essential
+    knife cookbook site download ohai
+    knife cookbook site download runit
+    knife cookbook site download yum-epel
+    knife cookbook site download rsyslog 2.0.0
+    knife cookbook site download packagecloud
+    knife cookbook site download yum
+    tar zxf *.gz
