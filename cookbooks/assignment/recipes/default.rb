c1dc4765834273c2427a6656fa0fb2c5c95b8f85
andy-do63 <andy.do63@gmail.com>
2016-01-05 14:18:55 -0500

    Add kitchen

--- cookbooks/assignment/recipes/default.rb
+++ cookbooks/assignment/recipes/default.rb
@@ -4,16 +4,4 @@
 # Copyright (c) 2015 Andy Do, All Rights Reserved
 #
 
-# Create shell script from files/default
-cookbook_file "/tmp/chef-init.sh" do
-    source "chef-init.sh"
-    mode 0755
-end
-
-# Execute Shell Script ONLY if not yet ran
-execute "Chef Init" do
-   command "sh /tmp/chef-init.sh"
-   
-   not_if do ::File.exists?('~/chef-repo/.chef/knife.rb') end
-end
-
+include_recipe "nginx"
---
+++ cookbooks/assignment/recipes/nginx.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: Assignment
+# Recipe:: default
+# Copyright (c) 2015 Andy Do, All Rights Reserved
+#
+
+# Create shell script from files/default
+cookbook_file "/tmp/chef-init.sh" do
+    source "chef-init.sh"
+    mode 0755
+end
+
+# Execute Shell Script ONLY if not yet ran
+execute "Chef Init" do
+   command "sh /tmp/chef-init.sh"
+   
+   not_if do ::File.exists?('~/chef-repo/.chef/knife.rb') end
+end
